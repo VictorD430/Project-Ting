e82ad4149c84cc4d66edf713880f6ba889c93327
Victor Duque <victor.duque11@hotmail.com>
2023-11-07 22:01:54 -0300

    erro lint

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,18 +2,18 @@ def exists_word(word, instance):
     word = word.lower()
     return_obj = []
     for index in range(len(instance)):
-        data_search = {}
+        data = {}
         for i in range(instance.search(index)["qtd_linhas"]):
             if word in instance.search(index)["linhas_do_arquivo"][i].lower():
-                if len(data_search) == 0:
-                    data_search["palavra"] = word
-                    data_search["arquivo"] = instance.search(index)["nome_do_arquivo"]
-                    data_search["ocorrencias"] = [{
+                if len(data) == 0:
+                    data["palavra"] = word
+                    data["arquivo"] = instance.search(index)["nome_do_arquivo"]
+                    data["ocorrencias"] = [{
                         "linha": i + 1,
                     }]
-                    return_obj.append(data_search)
+                    return_obj.append(data)
                 else:
-                    data_search["ocorrencias"].append({
+                    data["ocorrencias"].append({
                         "linha": i + 1,
                     })
     return return_obj
